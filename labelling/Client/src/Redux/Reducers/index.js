d31de24572d47179ecf34e8f09c1c224e6294c72
jpchoi <jpchoi@teslasystem.co.kr>
2022-09-16 23:35:25 +0900

    delete labelling

--- labelling/Client/src/Redux/Reducers/index.js
+++
@@ -1,6 +0,0 @@
-import SampleReducer from "./SampleReducer.js"
-import ImageLabelReducer from "./ImageLabelReducer"
-import VideoLabelReducer from "./VideoLabelReducer"
-import CommonReducer from "./CommonReducer"
-
-export { SampleReducer, ImageLabelReducer, VideoLabelReducer, CommonReducer }
