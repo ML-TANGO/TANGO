374fd0ce5310ed36d0fbcf7e4c59d3ad422eddb0
jpchoi <jpchoi@teslasystem.co.kr>
2022-09-16 23:19:35 +0900

    teslasystem - update

--- labelling/Client/src/Translations/index.js
+++
@@ -1,8 +0,0 @@
-import resources from "./resources"
-
-export const config = {
-  interpolation: { escapeValue: false }, // React already does escaping
-  lng: "ko",
-  //resources: { ko: { common: { dashboard: { title: "대쉬보드" } } } }
-  resources
-}
