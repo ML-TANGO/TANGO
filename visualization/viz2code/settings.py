15498221bf77ba4a15a570a094228f2a5bd8c8b6
Hongsoog Kim <hongsoog@gmail.com>
2022-10-04 15:26:05 +0900

    Subject: Modify viz2code middleware setting
    Details:
    - comment out 'django.middleware.clickjacking.XFrameOptionsMiddleware'
      in visualization/viz2code/settings.py for IFRAME inclusion
    - remove unnecessary *.py~HEAD files in the visualization
    
    To Do
    - labeling tool db update problem
      possible cause : pip does not installl version in requirement.txt
    
    [Related Issue: ]

--- visualization/viz2code/settings.py
+++ visualization/viz2code/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 # REST_FRAMEWORK = {
