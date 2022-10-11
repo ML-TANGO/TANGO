58652ee5288a6603344145f235ba8aebf98d8cca
Hyunwoo Cho <tenace@etri.re.kr>
2022-10-11 21:17:32 +0900

    Modify import errors

--- autonn/neck_nas/ku/models/common.py
+++ autonn/neck_nas/ku/models/common.py
@@ -4,7 +4,7 @@ import collections
 import torch
 import torch.nn as nn
 
-from mish_cuda import MishCuda as Mish
+# from mish_cuda import MishCuda as Mish
 
 
 def autopad(k, p=None):  # kernel, padding
@@ -27,7 +27,8 @@ class Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p),
                               groups=g, bias=False)
         self.bn = nn.BatchNorm2d(c2)
-        self.act = Mish() if act else nn.Identity()
+        # self.act = Mish() if act else nn.Identity()
+        self.act = nn.Mish() if act else nn.Identity()
 
     def forward(self, x):
         return self.act(self.bn(self.conv(x)))
@@ -61,7 +62,8 @@ class BottleneckCSP(nn.Module):
         self.cv3 = nn.Conv2d(c_, c_, 1, 1, bias=False)
         self.cv4 = Conv(2 * c_, c2, 1, 1)
         self.bn = nn.BatchNorm2d(2 * c_)  # applied to cat(cv2, cv3)
-        self.act = Mish()
+        # self.act = Mish()
+        self.act = nn.Mish()
         self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut,
                                             g, e=1.0) for _ in range(n)])
 
@@ -81,7 +83,8 @@ class BottleneckCSP2(nn.Module):
         self.cv2 = nn.Conv2d(c_, c_, 1, 1, bias=False)
         self.cv3 = Conv(2 * c_, c2, 1, 1)
         self.bn = nn.BatchNorm2d(2 * c_)
-        self.act = Mish()
+        # self.act = Mish()
+        self.act = nn.Mish()
         self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut,
                                             g, e=1.0) for _ in range(n)])
 
@@ -138,7 +141,8 @@ class SPPCSP(nn.Module):
         self.cv5 = Conv(4 * c_, c_, 1, 1)
         self.cv6 = Conv(c_, c_, 3, 1)
         self.bn = nn.BatchNorm2d(2 * c_)
-        self.act = Mish()
+        # self.act = Mish()
+        self.act = nn.Mish()
         self.cv7 = Conv(2 * c_, c2, 1, 1)
 
     def forward(self, x):
@@ -499,7 +503,8 @@ class ConvSqu(nn.Module):
         super(ConvSqu, self).__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p),
                               groups=g, bias=False)
-        self.act = Mish() if act else nn.Identity()
+        # self.act = Mish() if act else nn.Identity()
+        self.act = nn.Mish() if act else nn.Identity()
 
     def forward(self, x):
         return self.act(self.conv(x))
