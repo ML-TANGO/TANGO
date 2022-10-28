fa80701a960c1772e4be7b9d9da5e4e78601db2f
choijinpyo <jpchoi@teslasystem.co.kr>
2022-10-29 01:26:35 +0900

    project_manager

---
+++ project_manager/frontend/build/precache-manifest.61c8bd2d3e26a6dbb78b8d410ee5ee51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb6abf95f7073ebfb5bf",
+    "url": "/static/css/main.ed6e1562.chunk.css"
+  },
+  {
+    "revision": "cb6abf95f7073ebfb5bf",
+    "url": "/static/js/main.4de659c3.chunk.js"
+  },
+  {
+    "revision": "aab9dc3d1b7dfd49feb3",
+    "url": "/static/js/runtime~main.4f6ad734.js"
+  },
+  {
+    "revision": "5f43ee8757c900e6a32a",
+    "url": "/static/js/2.19539b22.chunk.js"
+  },
+  {
+    "revision": "f39c2c421cf3e946fed7",
+    "url": "/static/js/3.be1198cd.chunk.js"
+  },
+  {
+    "revision": "7498b0828c154b0b8ff9ec6c38b4f17a",
+    "url": "/static/media/data_th_1.7498b082.PNG"
+  },
+  {
+    "revision": "90d66baf4bf890cdd6f0dc5ed9ed8711",
+    "url": "/static/media/data_th_2.90d66baf.PNG"
+  },
+  {
+    "revision": "644e0cd1ad9b1a3195647e78b8590086",
+    "url": "/static/media/data_th_3.644e0cd1.PNG"
+  },
+  {
+    "revision": "0beac8aaffa6b55345a59db09b1cc6c5",
+    "url": "/static/media/data_th_4.0beac8aa.PNG"
+  },
+  {
+    "revision": "583a61e303192ee7d8cb40d45fbbb12c",
+    "url": "/static/media/Tango_login.583a61e3.png"
+  },
+  {
+    "revision": "81aed974f94d3f00ab894da8a4be1f46",
+    "url": "/static/media/Tango_main.81aed974.png"
+  },
+  {
+    "revision": "cbb293af9faedf817b5e8252b4fe85d5",
+    "url": "/static/media/bar-graph2x.cbb293af.png"
+  },
+  {
+    "revision": "37271b8b00b8463e3fbb4ba6a6593ed5",
+    "url": "/index.html"
+  }
+];
---
+++ project_manager/frontend/src/service/restContainerApi.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+
+import {server_ip, getHeaderData, getHeaderData_form, tokenExpiredCheck} from "./restApi_Base"
+
+/* Container - Start */
+export function requestContainerStart(container, uid, pid)
+{
+    let port = "";
+    switch(container)
+    {
+        case 'bms':
+            port = "8081";
+            break;
+        case 'viz':
+            port = "8091";
+            break;
+        case 'autonn_bb':
+            port = "8087";
+            break;
+        case 'autonn_nk':
+            port = "8089";
+            break;
+        case 'code_gen':
+            port = "8888";
+            break;
+        case 'deploy_cloud':
+            port = "8088";
+            break;
+        case 'deploy_ondevice':
+            port = "8891";
+            break;
+        default:
+            break;
+    }
+
+    return new Promise( (resolve, reject) =>
+    {
+        axios.get( "http://0.0.0.0:" + port + "/start", {
+            params: {
+                user_id: uid,
+                project_id: pid
+        }}).then((response) =>
+        {
+            resolve( response )
+        })
+        .catch(error =>
+        {
+            reject(error.response)
+        });
+    });
+}
+
+/* Container - status request */
+export function requestContainerStatusCheck(container, uid, pid)
+{
+    let port = "";
+    switch(container)
+    {
+        case 'bms':
+            port = "8081";
+            break;
+        case 'viz':
+            port = "8091";
+            break;
+        case 'autonn_bb':
+            port = "8087";
+            break;
+        case 'autonn_nk':
+            port = "8089";
+            break;
+        case 'code_gen':
+            port = "8888";
+            break;
+        case 'deploy_cloud':
+            port = "8088";
+            break;
+        case 'deploy_ondevice':
+            port = "8891";
+            break;
+        default:
+            break;
+    }
+
+    return new Promise( (resolve, reject) =>
+    {
+        axios.get( "0.0.0.0:"+ port + "/status_request", {
+            params: {
+                user_id: uid,
+                project_id: pid
+        }}).then((response) =>
+        {
+            resolve( response )
+
+            console.log(response)
+        })
+        .catch(error =>
+        {
+            reject(error.response)
+        });
+    });
+}
