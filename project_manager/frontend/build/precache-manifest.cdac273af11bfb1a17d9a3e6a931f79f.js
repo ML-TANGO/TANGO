5b3e36efb828229330169e0f8ab41f08373e294a
choijinpyo <jpchoi@teslasystem.co.kr>
2022-12-13 16:47:18 +0900

    dataset_load_update

---
+++ project_manager/frontend/build/precache-manifest.cdac273af11bfb1a17d9a3e6a931f79f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc43ae9f8a10c428f754",
+    "url": "/static/css/main.0d3d750c.chunk.css"
+  },
+  {
+    "revision": "bc43ae9f8a10c428f754",
+    "url": "/static/js/main.5dad360a.chunk.js"
+  },
+  {
+    "revision": "c7a672a6ae863f1eba66",
+    "url": "/static/js/runtime~main.e4ebbbcc.js"
+  },
+  {
+    "revision": "2721aca509cfcd55d88c",
+    "url": "/static/js/2.d5540464.chunk.js"
+  },
+  {
+    "revision": "294d1b401471ef833caf",
+    "url": "/static/js/3.67b4f5c1.chunk.js"
+  },
+  {
+    "revision": "583a61e303192ee7d8cb40d45fbbb12c",
+    "url": "/static/media/Tango_login.583a61e3.png"
+  },
+  {
+    "revision": "81aed974f94d3f00ab894da8a4be1f46",
+    "url": "/static/media/Tango_main.81aed974.png"
+  },
+  {
+    "revision": "cbb293af9faedf817b5e8252b4fe85d5",
+    "url": "/static/media/bar-graph2x.cbb293af.png"
+  },
+  {
+    "revision": "766134b421df92463a983a1c9a861ba1",
+    "url": "/index.html"
+  }
+];
