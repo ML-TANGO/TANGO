c8a6ddb21327565873182c850036243d1d93ae33
Ritarka Samanta <rsamanta9@rover.cc.gt.atl.ga.us>
2023-07-29 22:32:51 -0400

    filled yolov7 direcotory

---
+++ base_model_select/yolov7/generate_txt_file.py
@@ -0,0 +1,31 @@
+import linecache as lc
+import numpy as np
+from pathlib import Path
+
+txt_file_size = 5000
+target_size = 200
+
+
+randidx = np.random.randint(txt_file_size, size=target_size)
+
+# changed to global dataset location
+# print(Path().absolute())
+filename = "coco/val2017.txt"
+generatefile = 'evaluate2017.txt'
+
+# assert Path(generatefile).exists()
+assert Path(filename).exists()
+
+with open(generatefile, 'w+') as genfile:
+    
+    dataset_path = Path("coco")
+
+    for i in randidx.tolist():
+        line = lc.getline(filename, i)
+        path = (dataset_path / line).resolve()
+        genfile.writelines(str(path))
+        # print(str(path))
+
+
+    
+    
---
+++ base_model_select/yolov7/run.sh
@@ -0,0 +1,1 @@
+python predict.py --task evaluate --data coco.yaml --img 640 --iou 0.65 --half
