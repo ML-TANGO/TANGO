d0f1b395181a0ae2fdbe9bb38576b715dbb95143
Ritarka Samanta <rsamanta9@hummer.cc.gt.atl.ga.us>
2024-01-26 23:10:46 -0500

    fixed some merge conflicts

--- base_model_select/backend/views.py
+++ base_model_select/backend/views.py
@@ -63,26 +63,6 @@ def start(request):
     except models.Info.DoesNotExist:
         bmsinfo = models.Info(userid=userid, project_id=project_id)  
         print("new user or project")
-<<<<<<< HEAD
-    
-    data_yaml, proj_info_yaml = get_user_requirements(userid, project_id)
-    print(data_yaml, proj_info_yaml)
-	    
-    pr = mp.Process(target=task_to_model_mapping, args=(proj_info_yaml, userid, project_id), daemon=True)
-	     
-    pr_id = get_process_id()
-    PROCESSES[pr_id] = pr
-    print(f'{len(PROCESSES)}-th process is starting')
-    PROCESSES[pr_id].start()
-    
-    bmsinfo.proj_info_yaml=str(proj_info_yaml)
-    bmsinfo.data_yaml=str(data_yaml)
-    bmsinfo.status="started"
-    
-    bmsinfo.process_id = pr_id
-    bmsinfo.save()
-    return Response("started", status=200, content_type="text/plain")
-=======
 
     try:
         proj_info_yaml = get_user_requirements(userid, project_id)
@@ -106,7 +86,6 @@ def start(request):
         bmsinfo.status = "failed"
         bmsinfo.save()
         return Response("failed", status=200, content_type="text/plain")
->>>>>>> main
 
 
 @api_view(['GET'])
@@ -227,19 +206,6 @@ def create_basemodel_yaml(yaml_path, userid, project_id):
     with open(yaml_path, 'r') as f:
         proj_info = yaml.load(f, Loader=yaml.FullLoader)
     task = proj_info['task_type']
-<<<<<<< HEAD
-    target = proj_info['target_info']
-    model = task_to_model_table[task]
-    '''
-    proj_info['model_size'] = model_to_size_table[model][target]
-    with open(yaml_path, 'w') as f:
-        yaml.dump(proj_info, f, default_flow_style=False)
-    '''
-    model_size = model_to_size_table[model][target]
-    shutil.copy(f'basemodel_yaml/{model}/{model}{model_size}.yaml', f'/shared/common/{userid}/{project_id}/basemodel.yaml')
-    
-    status_report(userid, project_id, status="success")
-=======
     target = proj_info['target_info'].replace('-', '').replace('_', '').lower()
 
     model = task_to_model_table[task]
@@ -276,7 +242,6 @@ def get_process_id():     # Assign Blank Process Number
         except KeyError:
             break
     return pr_num
->>>>>>> main
 
 
 @api_view(['GET'])
@@ -343,20 +308,6 @@ def start_api(request):
     except models.Info.DoesNotExist:
         bmsinfo = models.Info(userid=userid, project_id=project_id)  
         print("new user or project")
-<<<<<<< HEAD
-        
-        # if bmsinfo.status != "ready":
-        #     print(f"existed user & project : {bmsinfo.status}... ignore this start signal")
-        #     return Response("error", status=200, content_type="text/plain")
-    
-        # for i in models.Info.objects.all():
-        #     if i.status != "ready":
-        #         print("not allow runnnig one more bms at the same time..."
-        #               " ignore this start signal")
-        #         return Response("error", status=200, content_type="text/plain")
-    
-=======
->>>>>>> main
 
     if request.method == 'GET':
         
@@ -378,97 +329,8 @@ def start_api(request):
         bmsinfo.process_id = pr_id
         bmsinfo.save()
         return Response("started", status=200, content_type="text/plain")
-<<<<<<< HEAD
-        
-        
-@api_view(['GET'])
-def stop_api(request):
-    print("_________GET /stop_____________")
-    params = request.query_params
-    userid = params['user_id']
-    project_id = params['project_id']
-    try:
-        bmsinfo = models.Info.objects.get(userid=userid, project_id=project_id)
-    except models.Info.DoesNotExist:
-        print("no such user or project...")
-        return Response('failed', status=200, content_type='text/plain')
-
-    PROCESSES[bmsinfo.process_id].terminate()
-    PROCESSES.pop(bmsinfo.process_id)
-    # bmsinfo.delete()
-    bmsinfo.status = "stopped"
-    bmsinfo.save()
-    return Response("stopped", status=200, content_type="text/plain")
-
-@api_view(['GET'])
-def status_request(request):
-    print("_________GET /status_request_____________")
-    params = request.query_params
-    userid = params['user_id']
-    project_id = params['project_id']
-    print(userid, project_id) 
-    try:
-        bmsinfo = models.Info.objects.get(userid=userid, project_id=project_id)
-        # if THREADS[bmsinfo.thread_id].is_alive():
-        print("process iD is", bmsinfo.process_id)
-        if PROCESSES[str(bmsinfo.process_id)].is_alive():
-            print("found thread running nas")
-            bmsinfo.status = "running"
-            bmsinfo.save()
-            return Response("running", status=200, content_type='text/plain')
-        else:
-            print("tracked bms you want, but not running anymore")
-            bmsinfo.status = "stopped"
-            bmsinfo.save()
-            return Response("stopped", status=200, content_type='text/plain')
-        
-    except models.Info.DoesNotExist:
-        # print("no such user or project...")
-        # return Response('failed', status=200, content_type='text/plain')
-        print("new user or project")
-        bmsinfo = models.Info(userid=userid, project_id=project_id)
-        bmsinfo.status = "ready"
-        bmsinfo.save()
-        return Response("ready", status=200, content_type='text/plain')
 
 
-def get_user_requirements(userid, projid):
-    common_root = Path('/shared/common/')
-    proj_path = common_root / userid / projid
-    proj_info_yaml_path = proj_path / 'project_info.yaml' # 'target.yaml'
-    dataset_yaml_path = proj_path / 'datasets.yaml'
-
-    return dataset_yaml_path, proj_info_yaml_path
-
-
-def status_report(userid, project_id, status="success"):
-    try:
-        url = 'http://projectmanager:8085/status_report'
-        headers = {
-            'Content-Type' : 'text/plain'
-        }
-        payload = {
-            'container_id' : "bms",
-            'user_id' : userid,
-            'project_id' : project_id,
-            'status' : status
-        }
-        response = requests.get(url, headers=headers, params=payload)
-        print(response.text)
-
-        bmsinfo = models.Info.objects.get(userid=userid,
-                                      project_id=project_id)
-        bmsinfo.status = "ready"
-        bmsinfo.save()
-        PROCESSES.pop(bmsinfo.process_id)
-        
-    except ValueError as e:
-        print(e)
-
-=======
-
-
->>>>>>> main
 def queue_bms(userid, project_id):
     try:
         # docker_run(userid, project_id)        
