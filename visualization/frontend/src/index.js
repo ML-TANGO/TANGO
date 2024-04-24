3147eb9285297db936db741176661ede240d11b5
Hyunwoo Cho <tenace@etri.re.kr>
2024-04-24 15:39:58 +0900

    Set CSRF cookie for axios

--- visualization/frontend/src/index.js
+++ visualization/frontend/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './App';
 import EditText from './EditText';
 import EditTextarea from './EditTextarea';
+
+axios.defaults.xsrfCookieName='csrftoken';
+axios.defaults.xsrfHeaderName='X-CSRFTOKEN';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 export { EditText, EditTextarea };
