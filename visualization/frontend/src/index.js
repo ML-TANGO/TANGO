3eb552b1fbbf1fec4210f951e246d64bcf7a4fef
Hyunwoo Cho <tenace@etri.re.kr>
2024-04-24 16:26:54 +0900

    Revert "Set CSRF cookie for axios"
    This reverts commit 3147eb9285297db936db741176661ede240d11b5.

--- visualization/frontend/src/index.js
+++ visualization/frontend/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import App from './App';
 import EditText from './EditText';
 import EditTextarea from './EditTextarea';
-
-axios.defaults.xsrfCookieName='csrftoken';
-axios.defaults.xsrfHeaderName='X-CSRFTOKEN';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 export { EditText, EditTextarea };
