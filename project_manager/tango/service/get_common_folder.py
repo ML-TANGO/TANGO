3f807d3a53b02e0df17845390a5cc9fda1fe3924
gmlee <gmlee@teslasystem.co.kr>
2024-09-02 17:00:57 +0900

    fix, get folder structure

--- project_manager/tango/service/get_common_folder.py
+++ project_manager/tango/service/get_common_folder.py
@@ -2,10 +2,14 @@ import os
 import glob
 
 from ..models import Project
+import traceback
 
 def get_project_name(project_id):
-    project_info = Project.objects.get(id = project_id)
-    return project_info.project_name
+    try:
+        project_info = Project.objects.get(id = project_id)
+        return project_info.project_name
+    except Exception as error:
+        return None
 
 def get_folder_structure(folder_path):
     try:
@@ -22,13 +26,18 @@ def get_folder_structure(folder_path):
                 for dir in dirs:
                     project_name = get_project_name(dir)
 
+                    if project_name == None:
+                        continue
+
                     if path_split[-1] not in project_id_to_name:
                         project_id_to_name[path_split[-1]] = {}
                     project_id_to_name[path_split[-1]][dir] = project_name
-
-                    dir = project_name
+                    # dir = project_name
                 
-                dirs = list(project_id_to_name[path_split[-1]].values())
+                try:
+                    dirs = list(project_id_to_name[path_split[-1]].values())
+                except Exception:
+                    dirs = []
 
             # 각 하위 Path의 project_id로 되어있는 경로를 project_name으로 변경
             if len(path_split) > 4 :
@@ -41,5 +50,7 @@ def get_folder_structure(folder_path):
         return structure
     except Exception as error:
         print("error", error)
+        traceback_message = traceback.format_exc()
+        print("ㅠㅠㅠ", traceback_message)
         return []
     
