a0674e4d2049867e4ba6cf817da5c8a5d6062e4b
Hyunwoo Cho <tenace@etri.re.kr>
2024-09-02 09:43:45 +0900

    Set env for PyTorch CUDA allocation config

--- autonn/autonn/autonn_core/tango/utils/general.py
+++ autonn/autonn/autonn_core/tango/utils/general.py
@@ -28,6 +28,7 @@ np.set_printoptions(linewidth=320, formatter={'float_kind': '{:11.5g}'.format}) 
 pd.options.display.max_columns = 10
 cv2.setNumThreads(0)  # prevent OpenCV from multithreading (incompatible with PyTorch DataLoader)
 os.environ['NUMEXPR_MAX_THREADS'] = str(min(os.cpu_count(), 8))  # NumExpr max threads
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
 DEBUG = False
 
