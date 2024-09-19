f1e973784b4eb93dd6aab216c1ada96a814e5f2d
gmlee <gmlee@teslasystem.co.kr>
2024-09-19 11:38:57 +0900

    update, Model Viz / Log Viz 더블 탭 구현

---
+++ project_manager/frontend/src/modules/project/log-viewer/AutonnLogViewer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="px-1">
+    <v-tabs v-model="tab" color="#eee">
+      <v-tabs-slider color="transparent"></v-tabs-slider>
+      <v-tab :key="'train'" :style="{ backgroundColor: getColor(0) }" class="mr-0 tab-border"> Train </v-tab>
+      <v-tab :key="'model'" :style="{ backgroundColor: getColor(1) }" class="ml-1 tab-border"> Model </v-tab>
+
+      <!-- <v-tab :key="'train'"> Train </v-tab>
+      <v-tab :key="'model'"> Model </v-tab> -->
+    </v-tabs>
+
+    <!-- <div style="width: 100%; height: 2px; background-color: #ccc" class="mb-3"></div> -->
+
+    <v-card class="pa-1 mt-2" color="#ddd">
+      <v-tabs-items v-model="tab">
+        <v-tab-item :key="'train'">
+          <LogWithChart :data="data" />
+        </v-tab-item>
+        <v-tab-item :key="'model'">
+          <ModelViewer />
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </div>
+</template>
+<script>
+import LogWithChart from "./LogWithChart.vue";
+import ModelViewer from "@/modules/project/model-viewer/ModelViewer.vue";
+export default {
+  components: { LogWithChart, ModelViewer },
+
+  props: {
+    data: {
+      default: null
+    }
+  },
+
+  data() {
+    return {
+      tab: null
+    };
+  },
+
+  computed: {
+    getColor() {
+      return currTab => {
+        if (this.tab === currTab) return "#4a80ff";
+        else return "#eee";
+      };
+    }
+  }
+};
+</script>
+<style lang="css" scoped>
+.tab-border {
+  border-radius: 4px;
+}
+</style>
