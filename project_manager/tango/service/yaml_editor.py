4272cfa7e8311c959414a7fbe3842f90a2fe019f
hskim <stevenkim1259@gmail.com>
2025-10-02 00:07:59 +0900

    update: add segmentation logic

--- project_manager/tango/service/yaml_editor.py
+++ project_manager/tango/service/yaml_editor.py
@@ -7,6 +7,8 @@ def get_hyperparameter_file_name(project_id):
 
         if project.task_type == TaskType.DETECTION:
             return "hyp.scratch.p5.yaml"
+        elif project.task_type == TaskType.SEGMENTATION:
+            return "hyp.scratch.seg.yaml"   
         elif project.task_type == TaskType.CLASSIFICATION:
             return "hyp.scratch.cls.yaml"
         
@@ -22,6 +24,8 @@ def get_arguments_file_name(project_id):
 
         if project.task_type == TaskType.DETECTION:
             return "args-detection.yaml"
+        elif project.task_type == TaskType.SEGMENTATION:
+            return "args-segmentation.yaml"
         elif project.task_type == TaskType.CLASSIFICATION:
             return "args-classification.yaml"
         
